8dae4ad434d6f7c4ff4d2768c079b64f58d56225
yigitopan <yigitopan@hotmail.com>
2022-11-16 10:39:43 +0300

    smooth scroll + service placeholder

--- src/components/ServiceDetail.vue
+++ src/components/ServiceDetail.vue
@@ -1,7 +1,20 @@
 <script setup>
-import { useRoute } from 'vue-router'
+import {useRouter, useRoute } from 'vue-router'
+import { ref} from 'vue'
 
 const route = useRoute()
+const router = useRouter()
+
+const serviceImgs = ref(null)
+
+const selectFromServiceList = (selected) => {
+    router.push(`/services/${selected}`)
+    window.scrollTo({
+        left: 0,
+        top: serviceImgs.value.offsetTop,
+        behavior: 'smooth'
+    });
+}
 </script>
 
 <style>
@@ -17,27 +30,54 @@ const route = useRoute()
 <template>
     <div class="container mx-auto">
         <div class="grid grid-cols-12">
-            <div class="col-span-12 sm:col-span-3">
-                <ul>
-                    <li class="p-4 bg-gray-200 mb-1 cursor-pointer">Malerarbeiten/Vollwärmeschutz</li>
-                    <li class="p-4 bg-gray-200 mb-1 cursor-pointer">Trockenbau</li>
-                    <li class="p-4 bg-gray-200 bg-s mb-1 cursor-pointer">Stuckateur Arbeiten</li>
-                    <li class="p-4 bg-gray-200 mb-1 cursor-pointer">Esstisch</li>
-                    <li class="p-4 bg-gray-200 mb-1 cursor-pointer">Fliesen</li>
-                    <li class="p-4 bg-gray-200 cursor-pointer">Außenanlagen</li>
+            <div class="col-span-12 py-8 sm:py-0 sm:col-span-3">
+                <ul class="text-center font-semibold sm:text-left ease-out">
+                    <li @click="selectFromServiceList('malerarbeiten')" class="hover:bg-mainColor p-4 bg-gray-200 mb-1 cursor-pointer">Malerarbeiten</li>
+                    <li @click="selectFromServiceList('trockenbau')" class="hover:bg-mainColor p-4 bg-gray-200 mb-1 cursor-pointer">Trockenbau</li>
+                    <li @click="selectFromServiceList('stuckateur')" class="hover:bg-mainColor p-4 bg-gray-200 bg-s mb-1 cursor-pointer">Stuckateur Arbeiten</li>
+                    <li @click="selectFromServiceList('esstisch')" class="hover:bg-mainColor p-4 bg-gray-200 mb-1 cursor-pointer">Esstisch</li>
+                    <li @click="selectFromServiceList('fliesen')" class="hover:bg-mainColor p-4 bg-gray-200 mb-1 cursor-pointer">Fliesen</li>
+                    <li @click="selectFromServiceList('aussenanlagen')" class="hover:bg-mainColor p-4 bg-gray-200 cursor-pointer">Außenanlagen</li>
                 </ul>
             </div>
 
-            <div class="col-span-12 sm:col-span-9">
-                <div class="flex mb-5 px-8 gap-4 flex-row w-full">
+            <div ref="serviceImgs" class="col-span-12 sm:col-span-9">
+                <div v-if="route.params.serviceName=='fliesen'" class="flex mb-5 sm:px-8 gap-4 flex-row w-full">
                     <div class="bg-serviceDemo1 bg-no-repeat bg-center bg-cover w-1/2 h-64"></div>
                     <div class="bg-serviceDemo2 bg-no-repeat bg-center bg-cover w-1/2 h-64"></div>
                 </div>
 
+                <div v-else class="flex mb-5 sm:px-8 gap-4 flex-row w-full">
+                    <div class="bg-serviceDemo2 bg-no-repeat bg-center bg-cover w-1/2 h-64"></div>
+                    <div class="bg-serviceDemo1 bg-no-repeat bg-center bg-cover w-1/2 h-64"></div>
+                </div>
+
                 <div class="px-8">
-                    <h2 class="font-medium service-subtitle text-2xl pb-3 pt-3">SERVICE DETAIL {{route.params.serviceName}}</h2>
-                    <p class="font-thin">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora minima totam, ratione quas ipsum beatae, tenetur repudiandae eius doloribus, ipsa aut est sed doloremque voluptatibus sapiente voluptate odio minus eveniet.
-                    Et est maxime odit neque blanditiis fugit natus officiis cumque. Repellendus quod neque ab architecto, dolor quaerat voluptas commodi quas quam tempore ex maxime velit suscipit atque omnis, error accusantium.</p>
+                    <h2 class="font-medium service-subtitle text-2xl pb-3 pt-3 uppercase">{{route.params.serviceName}}</h2>
+                    <div v-if="route.params.serviceName=='fliesen'" >
+                        <p class="font-thin">Fliesen Text. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora minima totam, ratione quas ipsum beatae, tenetur repudiandae eius doloribus, ipsa aut est sed doloremque voluptatibus sapiente voluptate odio minus eveniet.
+                        Et est maxime odit neque blanditiis fugit natus officiis cumque. Repellendus quod neque ab architecto, dolor quaerat voluptas commodi quas quam tempore ex maxime velit suscipit atque omnis, error accusantium.</p>
+                        <br>
+                        <p class="font-thin">Fliesen Text. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora minima totam, ratione quas ipsum beatae, tenetur repudiandae eius doloribus, ipsa aut est sed doloremque voluptatibus sapiente voluptate odio minus eveniet.
+                        Et est maxime odit neque blanditiis fugit natus officiis cumque. Repellendus quod neque ab architecto, dolor quaerat voluptas commodi quas quam tempore ex maxime velit suscipit atque omnis, error accusantium.</p>
+                    </div>
+                   
+                    <div v-else-if="route.params.serviceName=='aussenanlagen'" >
+                        <p class="font-thin">aussenanlagen Text. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora minima totam, ratione quas ipsum beatae, tenetur repudiandae eius doloribus, ipsa aut est sed doloremque voluptatibus sapiente voluptate odio minus eveniet.
+                        Et est maxime odit neque blanditiis fugit natus officiis cumque. Repellendus quod neque ab architecto, dolor quaerat voluptas commodi quas quam tempore ex maxime velit suscipit atque omnis, error accusantium.</p>
+                        <br>
+                        <p class="font-thin">aussenanlagen Text. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora minima totam, ratione quas ipsum beatae, tenetur repudiandae eius doloribus, ipsa aut est sed doloremque voluptatibus sapiente voluptate odio minus eveniet.
+                        Et est maxime odit neque blanditiis fugit natus officiis cumque. Repellendus quod neque ab architecto, dolor quaerat voluptas commodi quas quam tempore ex maxime velit suscipit atque omnis, error accusantium.</p>
+                    </div>
+
+                    <div v-else >
+                        <p class="font-thin">Else Text. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora minima totam, ratione quas ipsum beatae, tenetur repudiandae eius doloribus, ipsa aut est sed doloremque voluptatibus sapiente voluptate odio minus eveniet.
+                        Et est maxime odit neque blanditiis fugit natus officiis cumque. Repellendus quod neque ab architecto, dolor quaerat voluptas commodi quas quam tempore ex maxime velit suscipit atque omnis, error accusantium.</p>
+                        <br>
+                        <p class="font-thin">Else Text. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora minima totam, ratione quas ipsum beatae, tenetur repudiandae eius doloribus, ipsa aut est sed doloremque voluptatibus sapiente voluptate odio minus eveniet.
+                        Et est maxime odit neque blanditiis fugit natus officiis cumque. Repellendus quod neque ab architecto, dolor quaerat voluptas commodi quas quam tempore ex maxime velit suscipit atque omnis, error accusantium.</p>
+                    </div>
+
                 </div>
             </div>
         </div>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -4,7 +4,7 @@ import { useRouter, useRoute } from 'vue-router'
 const router = useRouter()
 
 const serviceHeroClick = (selected) => {
-    router.push('/services/construction')
+    router.push(`/services/${selected}`)
 }
 
 </script>
@@ -26,32 +26,32 @@ const serviceHeroClick = (selected) => {
     <div class="container mx-auto">
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-10">
             <div>
-                <div @click="serviceHeroClick(1)"  class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_01.jpg" alt="" style="display: block;"/></div>
+                <div @click="serviceHeroClick('malerarbeiten')"  class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_01.jpg" alt="" style="display: block;"/></div>
                 <div><h3 class="tracking-wide uppercase service-main-title text-lg font-extrabold text-center py-3">Malerarbeiten/Vollwärmeschutz</h3></div>
                 <div><p class="px-3 text-center font-light">We can help you bring new life to existing rooms and develop unused spaces.</p></div>
             </div>
             <div>
-                <div @click="serviceHeroClick(2)" class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_02.jpg" alt="" style="display: block;"/></div>
+                <div @click="serviceHeroClick('trockenbau')" class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_02.jpg" alt="" style="display: block;"/></div>
                 <div><h3 class="tracking-wide uppercase service-main-title text-lg font-extrabold text-center py-3">Trockenbau</h3></div>
                 <div><p class="px-3 text-center font-light">We can help you bring new life to existing rooms and develop unused spaces.</p></div>
             </div>
             <div>
-                <div @click="serviceHeroClick('construction')" class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_03.jpg" alt="" style="display: block;"/></div>
+                <div @click="serviceHeroClick('stuckateur')" class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_03.jpg" alt="" style="display: block;"/></div>
                 <div><h3 class="tracking-wide uppercase service-main-title text-lg font-extrabold text-center py-3">Stuckateur Arbeiten</h3></div>
                 <div><p class="px-3 text-center font-light">We can help you bring new life to existing rooms and develop unused spaces.</p></div>
             </div>
             <div>
-                <div @click="serviceHeroClick(4)" class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_04.jpg" alt="" style="display: block;"/></div>
+                <div @click="serviceHeroClick('esstisch')" class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_04.jpg" alt="" style="display: block;"/></div>
                 <div><h3 class="tracking-wide uppercase service-main-title text-lg font-extrabold text-center py-3">Esstisch</h3></div>
                 <div><p class="px-3 text-center font-light">We can help you bring new life to existing rooms and develop unused spaces.</p></div>
             </div>
             <div>
-                <div @click="serviceHeroClick(5)" class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_05.jpg" alt="" style="display: block;"/></div>
+                <div @click="serviceHeroClick('fliesen')" class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_05.jpg" alt="" style="display: block;"/></div>
                 <div><h3 class="tracking-wide uppercase service-main-title text-lg font-extrabold text-center py-3">Fliesen</h3></div>
                 <div><p class="px-3 text-center font-light">We can help you bring new life to existing rooms and develop unused spaces.</p></div>
             </div>
             <div>
-                <div @click="serviceHeroClick(6)" class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_06.jpg" alt="" style="display: block;"/></div>
+                <div @click="serviceHeroClick('aussenanlagen')" class="cursor-pointer transition ease-in-out hover:contrast-50"><img class="w-full" src="https://quanticalabs.com/Renovate/Template/images/samples/390x260/image_06.jpg" alt="" style="display: block;"/></div>
                 <div><h3 class="tracking-wide uppercase service-main-title text-lg font-extrabold text-center py-3">Außenanlagen</h3></div>
                 <div><p class="px-3 text-center font-light">We can help you bring new life to existing rooms and develop unused spaces.</p></div>
             </div>
