8130bd26d89ca9f42740123af259b360c98d851f
Arif Erdem Avci <maykilscofieldd@gmail.com>
2022-11-16 16:21:07 +0300

    arif 16.11 4:21pm

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,18 @@ const emits = defineEmits('emit-name');
 </script>
 
 <template>
-    <header class="text-center py-10 bg-gray-100">
-    {{ title }}
-  </header>
+  <div class="flex justify-between">
+   <div>
+      <img class="-mr-1 ml-2 h-10 w-10" src="../assets/firmlogo-removebg-preview.png">
+   </div>
+   <div>
+ <router-link class="shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50" to="/">Home</router-link>
+ <router-link class="shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50" to="/about">
+   About
+ </router-link>
+ <router-link class="shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50" to="/contact">Contact</router-link>
+ <router-link class="shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50" to="/services">Services</router-link>
+</div>
+</div>
+ 
 </template>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,18 +1,55 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import * as vue from 'vue';
 
-const userData = reactive({
+const userData = vue.reactive({
     name: '',
     nameValid: true,
-    email : ''
+    email : '',
+    phone : '',
+    message : ''
 })
 </script>
 
 <template>
+    <div class="my-10">
+        <div class="columns-3">
+            <ul>
+                <li>
+                    <img src="../assets/location-sign-svgrepo-com.svg" class = "w-5 h-5 my-2">
+
+
+                </li>
+                
+                <p>Street</p>
+            </ul>
+            <ul>
+                <li>
+                    <img src="../assets/phone-svgrepo-com.svg" class = "w-5 h-5 my-2">
+                    
+                </li>
+                <p>+40 2525234 </p>
+            </ul>
+            <ul>
+                <li><img src="../assets/clock-svgrepo-com.svg" class = "w-5 h-5 my-2"></li>
+                <p>Mon-Fri 09:00-17:00</p>
+            </ul>
+
+        </div>
+
+    </div>
 <div>
-    <input type="text" class="border rounded m-4" :class="{ 'border border-red-500': !userData.nameValid }" v-model="userData.name" />
-    <input type="text" class="border rounded m-4" v-model="userData.email" :class="{ 'border-2 border-red-500': userData.email.length < 3 }" />
-    <button type="button" @click="userData.name = 'Yigit'; userData.nameValid = false">change name to yiigt</button>
+    <div class = "row-2">
+        <fieldset class = "columns-2 justify-between">
+    <input type="text" class="h-20 w-20 border rounded m-8" placeholder = "Your Name: " :class="{ 'border border-red-500': !userData.nameValid }" v-model="userData.name" />
+    <input type="text" class="h-20 w-20 border rounded m-8" placeholder = "Your Email: " v-model="userData.email" :class="{ 'border-2 border-red-500': userData.email.length < 3 }" />
+    <input type="text" class="h-20 w-40 border rounded m-8" placeholder = "Your Phone: " v-model="userData.phone" :class="{ 'border-2 border-red-500': userData.phone.length < 3 }" />
+</fieldset>
+<fieldset class = "columns-2">
+    <textarea type="text" class="border rounded m-4" placeholder = "Your Message: " v-model="userData.message" :class="{ 'border-2 border-red-500': userData.message.length < 3 }" />
+</fieldset>
+</div>
+    <!-- <button type="button" @click="userData.name = 'Yigit'; userData.nameValid = false">Send Your Message</button> -->
+    <input type="submit" name="submit" value="SEND MESSAGE" class="bg-yellow-500 hover:bg-yellow-700 text-black font-sans py-2 px-4 rounded-full">
     <p>{{  userData.name }}</p>
 </div>
 </template>
