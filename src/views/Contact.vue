19a5be0756930f371bb94980fbfd13bd90cf52fa
aeavci8 <maykilscofieldd@gmail.com>
2022-11-24 14:42:52 +0300

    Contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,93 +1,114 @@
 <script setup>
-import * as vue from 'vue';
+import * as vue from "vue";
 
 const userData = vue.reactive({
-    name: '',
-    nameValid: true,
-    email : '',
-    phone : '',
-    message : ''
-})
+  name: "",
+  nameValid: true,
+  email: "",
+  phone: "",
+  message: "",
+});
 </script>
 
 <style>
- @media (max-width : 768px){
-        .iframe-wrapper {
-        top: -100px!important;
-    }
+@media (max-width: 768px) {
+  .iframe-wrapper {
+    top: -100px !important;
+  }
 }
 
 .iframe-wrapper {
-        top: -50px;
+  top: -50px;
 }
 </style>
 
 <template>
-    <div class="relative w-full z-10">
-        <div class="absolute overflow-hidden w-full iframe-wrapper">
-            <iframe src="https://www.google.com/maps/d/embed?mid=1nUpg6rN6Ico2e3fkJ-LzKN4O4rJGJKI&ehbc=2E312F" width="100%" height="350px"></iframe>
-        </div>
+  <div class="relative w-full z-10">
+    <div class="absolute overflow-hidden w-full iframe-wrapper">
+      <iframe
+        src="https://www.google.com/maps/d/embed?mid=1nUpg6rN6Ico2e3fkJ-LzKN4O4rJGJKI&ehbc=2E312F"
+        width="100%"
+        height="350px"
+      ></iframe>
     </div>
-    <div class="mt-96 bg-mainColor">
-        <div class="grid grid-cols-3 gap-x-16 gap-y-8">
-                <div class="rows-span-2 gap-x-16">
-                    <div class = "flex justify-center"><img src="../assets/location-sign-svgrepo-com.svg" class="w-7 h-7"></div>
-                    <div class="flex relative">
-                        <p class = "text-center w-full">Robert Bosch Straße
-74196 Neuenstadt am Kocher
-</p>
-                    </div>
-              
-                </div>
-
-               
-                
-               <div class="row-span-2">
-                    <div class = "flex justify-center">
-                        <img src="../assets/phone-svgrepo-com.svg" class="w-7 h-7">
-                    </div>
-                    
-               <div>
-                 <p class = "text-center w-full">+40 2525234 </p>
-               </div>
-        
-            </div>
-          
-           <div class = "row-span-2">
-                <div class = "flex justify-center">
-                    <img src="../assets/clock-svgrepo-com.svg" class="w-7 h-7">
-                </div>
-               <div>
-                 <p class = "text-center w-full">Mon-Fri 09:00-17:00</p>
-               </div>
-          
-            </div>
-            
-
+  </div>
+  <div class="mt-80 flex w-full min-h-screen justify-center items-center">
+    <div
+      class="flex flex-col md:flex-row md:space-x-6 md:space-y-0 space-y-6 bg-yellow-700 w-full max-w-4xl p-8 sm:p-12 rounded-xl shadow-lg text-white"
+    >
+      <div class="flex flex-col space-y-8 justify-between">
+        <div>
+          <h1 class="font-bold text-4xl tracking-wide">Contact Us</h1>
+          <p class="pt-2 text-cyan-100 text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos,
+            reprehenderit molestiae?
+          </p>
         </div>
-
+        <div class="flex flex-col space-y-4">
+          <div class="inline-flex space-x-2 items-center">
+            <img
+              src="../assets/location-sign-svgrepo-com.svg"
+              class="w-7 h-7"
+            />
+            <span class="">
+              Robert Bosch Straße 74196 Neuenstadt am Kocher
+            </span>
+          </div>
+          <div class="inline-flex space-x-2 items-center">
+            <img src="../assets/phone-svgrepo-com.svg" class="w-7 h-7" />
+            <span class="">+40 2525234</span>
+          </div>
+          <div class="inline-flex space-x-2 items-center">
+            <img src="../assets/clock-svgrepo-com.svg" class="w-7 h-7" />
+            <span class="">Mon-Fri 09:00-17:00</span>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div class="bg-white rounded-xl shadow-lg p-8 text-gray-600 md:w-96">
+        <form class="flex flex-col space-y-4">
+          <div>
+            <label class="text-sm">Your Name</label>
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Your Name"
+              class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300"
+            />
+          </div>
+          <div>
+            <div>
+              <label class="text-sm">Your Email</label>
+            </div>
+            <div>
+              <input
+                type="email"
+                placeholder="Your Email"
+                class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300"
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label class="text-sm">Message</label>
+            </div>
+            <div>
+              <textarea
+                type="email"
+                rows="4"
+                placeholder="Message"
+                class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300"
+              ></textarea>
+            </div>
+          </div>
+          <button
+            class="inline-block self-end bg-mainColor text-white font-bold rounded-lg px-6 py-2 uppercase text-sm"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
     </div>
-<div>
-    <div class = "mt-10 row-2">
-        <p class="text-mainColor text-center">Contact Us</p>
-        <fieldset class = "row-3 flex flex-col items-center">
-            <div class = "m-2 w-1/5 min-w-fit">
-    <input type="text" class="h-10 w-60 border rounded font-extralight border-gray-400" placeholder = "Your Name" :class="{ 'border border-red-500': !userData.nameValid }" v-model="userData.name" /></div>
-    <div class= "m-2 w-1/5 min-w-fit">
-    <input type="text" class="h-10 w-60 border rounded font-extralight border-gray-400" placeholder = "Your Email" v-model="userData.email" :class="{ 'border-2 border-red-500': userData.email.length < 3 }" />
-</div>
-<div class= "m-2 w-1/5 min-w-fit">
-    <input type="text" class="h-10 w-60 border rounded font-extralight border-gray-400" placeholder = "Your Phone" v-model="userData.phone" :class="{ 'border-2 border-red-500': userData.phone.length < 3 }" /></div>
-</fieldset>
-<fieldset class = "m-2 flex flex-col items-center min-w-fit">
-    <textarea type="text" class="h-40 w-96 border rounded font-extralight border-gray-400" placeholder = "Your Message" v-model="userData.message" :class="{ 'border-2 border-red-500': userData.message.length < 3 }" />
-</fieldset>
-</div>
-
-</div>
-<div class = "flex justify-center">
-    <!-- <button type="button" @click="userData.name = 'Yigit'; userData.nameValid = false">Send Your Message</button> -->
-    <input type="submit" name="submit" value="SEND MESSAGE" class="bg-mainColor hover:bg-yellow-700 text-black font-sans m-8 py-2 px-4 rounded-full">
-
-</div>
+  </div>
 </template>
